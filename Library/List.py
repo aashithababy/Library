20bedbf8b1ef4b1386b62a301fb08085b58be580
aashitha <aashithababy1993@gmail.com>
2024-11-25 17:18:22 +0530

    Created a Project for Library Management

---
+++ Library/List.py
@@ -0,0 +1,8 @@
+class Library:
+    def list(self):
+        p=emp()
+        print(p.name)
+        print(p.age)
+        print(p.doj)
+        print(p.createdOn)
+        print(p.isActive)
---
+++ Models/Employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self,name,age,doj,createdOn,isActive):
+        self.name=name
+        self.age=age
+        self.doj=doj
+        self.createdOn=createdOn
+        self.isActive=isActive
+
+    
